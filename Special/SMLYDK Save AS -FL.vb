9502e37a4d35c040f69097a8b28faf6b4b1d32ba
Cody <Codered741@gmail.com>
2019-12-13 10:44:44 -0500

    misc updates

--- Special/SMLYDK Save AS -FL.vb
+++ Special/SMLYDK Save AS -FL.vb
@@ -5,158 +5,160 @@ Imports System.IO
 'http://adndevblog.typepad.com/manufacturing/2015/01/add-external-ilogic-rule-to-event-trigger.html
 
 Sub Main
-	Dim TemplateFilePath as String = "C:\_vaultWIP\Designs\Templates\Universal\US_FLORIDA\_USF_STANDARD TEMPLATE_SIZE B_11x17.idw"
-	Dim TBName as String = "UNIVERSAL FLORIDA MAIN SHEET"
-	Dim BdrName as String = "UNIVERSAL BORDER - FULL"
-	Dim oTemplateDoc As Document
-	Dim oTemplate As String 
-	Dim oDoc as Document = ThisApplication.ActiveDocument
-	
-	If oDoc.DocumentType = kDrawingDocumentObject Then
-		oTemplate = TemplateFilePath 'file to copy events from
-	'ElseIf oDoc.DocumentType = kPartDocumentObject Then
-		'oTemplate = "\\server\departments\CAD\Inventor\Templates\Standard.ipt"   'file to copy events from
-	Else
-		'oTemplate = "\\server\departments\CAD\Inventor\Templates\Standard.iam"   'file to copy events from
-		MsgBox("Must run from drawing")
-		Exit Sub
-	End If
-	
-	'ADDED For SMLYDK to create -FL documents
-	oFileName = DocumentFileName(oDoc)
-	FLPath = InputBox("Please enter the file path you would like to save to: ", "Save Path", _ 
-		Left(oDoc.FullFileName, oDoc.FullFileName.Length - DocumentFileNameExt(oDoc).Length))
 
-	If FLPath = vbCancel Then
-		MsgBox("You did not make a selection.  Exiting")
-		Exit Sub
-	End If 
+	msgbox("This rule is obsolete, and may not be used.  ")
+	' Dim TemplateFilePath as String = "C:\_vaultWIP\Designs\Templates\Universal\US_FLORIDA\_USF_STANDARD TEMPLATE_SIZE B_11x17.idw"
+	' Dim TBName as String = "UNIVERSAL FLORIDA MAIN SHEET"
+	' Dim BdrName as String = "UNIVERSAL BORDER - FULL"
+	' Dim oTemplateDoc As Document
+	' Dim oTemplate As String 
+	' Dim oDoc as Document = ThisApplication.ActiveDocument
 	
-	FLName = FLPath & "\" & oFileName & "-FL.dwg"
-	oDoc.SaveAs(FLName, False)
-	
-	oDoc = ThisApplication.Documents.ItemByName(FLName)
-	
-	' Dim oModelDoc as Document' = oDoc.AllReferencedDocuments.Item(1)
-	
-	' Try
-		' oModelDoc = oDoc.AllReferencedDocuments.Item(1)
-	' Catch 
-		' MessageBox.Show("There are no models referenced in this drawing.  The Rule cannot continue.  ", "No Model", MessageBoxButtons.OK, MessageBoxIcon.Error)
+	' If oDoc.DocumentType = kDrawingDocumentObject Then
+		' oTemplate = TemplateFilePath 'file to copy events from
+	' 'ElseIf oDoc.DocumentType = kPartDocumentObject Then
+		' 'oTemplate = "\\server\departments\CAD\Inventor\Templates\Standard.ipt"   'file to copy events from
+	' Else
+		' 'oTemplate = "\\server\departments\CAD\Inventor\Templates\Standard.iam"   'file to copy events from
+		' MsgBox("Must run from drawing")
 		' Exit Sub
-	' End Try
+	' End If
 	
-	'If the first reference is not a part or assembly, find the first assembly/part in the list.  
-	' If oModelDoc.DocumentType <> kAssemblyDocumentObject OrElse oModelDoc.DocumentType <> kPartDocumentObject 
-		' For docnum = 1 to oDoc.AllReferencedDocuments.Count ' oModelDoc.DocumentType <> kAssemblyDocumentObject OrElse oModelDoc.DocumentType <> kPartDocumentObject
-			' oModelDoc = oDoc.AllReferencedDocuments.Item(docnum)
-			' ModelFileName = Right(oModelDoc.FullFileName, oModelDoc.FullFileName.Length - InStrRev(oModelDoc.FullFileName, "\"))
-			' 'msgbox(ModelFileName)
-			' If ModelFileName.Contains(oDoc.PropertySets.Item("Design Tracking Properties").Item("Part Number").Value) AndAlso oModelDoc.DocumentType <> kPresentationDocumentObject Then  'DocumentType = kAssemblyDocumentObject OrElse oModelDoc.DocumentType = kPartDocumentObject
-				' 'MsgBox(oModelDoc.FullFileName)
-				' Exit For
+	' 'ADDED For SMLYDK to create -FL documents
+	' oFileName = DocumentFileName(oDoc)
+	' FLPath = InputBox("Please enter the file path you would like to save to: ", "Save Path", _ 
+		' Left(oDoc.FullFileName, oDoc.FullFileName.Length - DocumentFileNameExt(oDoc).Length))
+
+	' If FLPath = vbCancel Then
+		' MsgBox("You did not make a selection.  Exiting")
+		' Exit Sub
+	' End If 
+	
+	' FLName = FLPath & "\" & oFileName & "-FL.dwg"
+	' oDoc.SaveAs(FLName, False)
+	
+	' oDoc = ThisApplication.Documents.ItemByName(FLName)
+	
+	' ' Dim oModelDoc as Document' = oDoc.AllReferencedDocuments.Item(1)
+	
+	' ' Try
+		' ' oModelDoc = oDoc.AllReferencedDocuments.Item(1)
+	' ' Catch 
+		' ' MessageBox.Show("There are no models referenced in this drawing.  The Rule cannot continue.  ", "No Model", MessageBoxButtons.OK, MessageBoxIcon.Error)
+		' ' Exit Sub
+	' ' End Try
+	
+	' 'If the first reference is not a part or assembly, find the first assembly/part in the list.  
+	' ' If oModelDoc.DocumentType <> kAssemblyDocumentObject OrElse oModelDoc.DocumentType <> kPartDocumentObject 
+		' ' For docnum = 1 to oDoc.AllReferencedDocuments.Count ' oModelDoc.DocumentType <> kAssemblyDocumentObject OrElse oModelDoc.DocumentType <> kPartDocumentObject
+			' ' oModelDoc = oDoc.AllReferencedDocuments.Item(docnum)
+			' ' ModelFileName = Right(oModelDoc.FullFileName, oModelDoc.FullFileName.Length - InStrRev(oModelDoc.FullFileName, "\"))
+			' ' 'msgbox(ModelFileName)
+			' ' If ModelFileName.Contains(oDoc.PropertySets.Item("Design Tracking Properties").Item("Part Number").Value) AndAlso oModelDoc.DocumentType <> kPresentationDocumentObject Then  'DocumentType = kAssemblyDocumentObject OrElse oModelDoc.DocumentType = kPartDocumentObject
+				' ' 'MsgBox(oModelDoc.FullFileName)
+				' ' Exit For
+			' ' End If
+		' ' Next
+	' ' End If
+	
+	' ' If oModelDoc.DocumentType = kPartDocumentObject Then
+			' ' oModelDoc.PropertySets.Item("Design Tracking Properties").Item("Project").Value = ""
+	' ' End If
+		
+	' 'Dim oCtrlDef as ControlDefinitions = ThisApplication.CommandManager.ControlDefinitions
+	' 'oCtrlDef.Item("VaultCheckoutTop").Execute
+	
+	' SharedVariable("LogVar") = "SMLYDK Save as -FL"
+	' iLogicVB.RunExternalRule("Write SV to Log.iLogicVB")
+	
+	' 'check file type 
+	
+	
+	' Dim oOptions as NameValueMap = ThisApplication.TransientObjects.CreateNameValueMap
+		' oOptions.Value("DeferUpdates") = True
+		' oOptions.Value("FastOpen") = True
+		
+	' 'open the template
+	' oTemplateDoc = ThisApplication.Documents.OpenWithOptions(oTemplate, oOptions, False)
+	
+	' TitleBlockCopy(oTemplateDoc, oDoc, TBName)
+	' 'TitleBlockCopy(oTemplateDoc, oDoc, "UNIVERSAL BOURNE Kit Add")
+	' BorderCopy(oTemplateDoc, oDoc, BdrName)
+	' ' SymbolCopy(oTemplateDoc, oDoc, "ACAD Leader Right")
+	' ' SymbolCopy(oTemplateDoc, oDoc, "ACAD Leader Left")
+	' ' SymbolCopy(oTemplateDoc, oDoc, "Univ. Part Notes")
+	' ' SymbolCopy(oTemplateDoc, oDoc, "TubeWeldTyp")
+	
+	' ' Dim IsKit = vbNo
+	' ' IsKit = MessageBox.Show("Is this a kit?","Kit?", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
+	' ' Try 
+		' ' If oModelDoc.PropertySets.Item("Inventor User Defined Properties").Item("DesignLevel") = "Kit" _
+		 ' ' OrElse oModelDoc.PropertySets.Item("Inventor User Defined Properties").Item("DesignLevel") = "Element" Then
+			' ' IsKit = MessageBox.Show("Is this a kit?","Kit?", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
+		' ' End If 
+	' ' Catch
+	' ' End Try
+	
+	' Dim oSheets As Sheets = oDoc.Sheets
+	' Dim oSheet as Sheet = oDoc.Sheets.Item(1)
+	' ' Dim SheetCount = 1
+	
+	' ' 'if a kit, copy the general notes page, delete the current, and move the newly copied sheet to sheet 2
+	' ' If IsKit = vbYes Then
+		' ' oTemplateDoc.Sheets.Item(2).CopyTo(oDoc)
+		' ' oSheets.Item(2).Delete
+		
+		' ' Dim oBrowserPane as BrowserPane = oDoc.BrowserPanes.Item("Model")
+		' ' Dim ShtGenNotesNew As Sheet = oSheets.Item(oSheets.Count)
+		' ' Dim sheetNode As BrowserNode = oBrowserPane.GetBrowserNodeFromObject(ShtGenNotesNew)
+		' ' oBrowserPane.Reorder(oBrowserPane.GetBrowserNodeFromObject(oSheet), False, sheetNode)
+		
+	' ' End If
+	
+	' oTemplateDoc.Close(True)
+	
+	' oSheet.Activate
+	
+	' Dim oBorderDef as BorderDefinition = oDoc.BorderDefinitions.Item(BdrName)
+	' Dim oTitleTBDef as TitleBlockDefinition = oDoc.TitleBlockDefinitions.Item(TBName)
+	' 'Dim oAddPageDef as TitleBlockDefinition = oDoc.TitleBlockDefinitions.Item("UNIVERSAL BOURNE Kit Add")
+	
+	' oSheet.Border.Delete
+	' oSheet.TitleBlock.Delete
+	' oSheet.AddBorder(oBorderDef)
+	' oSheet.AddTitleBlock(oTitleTBDef)
+
+	' ' If oDoc.AllReferencedDocuments.Item(1).DocumentType = kPartDocumentObject Then	
+		' ' InsertSketchedSymbolOnSheet(oDoc, "Univ. Part Notes")
+	' ' End If
+	
+	' If oSheets.Count > 1 Then
+		
+		' For Each oSheet In oSheets
+			' If SheetCount < oSheets.Count Then
+				
+				' SheetCount = SheetCount + 1
+				' oSheet = oDoc.Sheets.Item(SheetCount)
+				' oSheet.Activate
+				' oSheet.Border.Delete
+				' oSheet.AddBorder(oBorderDef)
+				' oSheet.TitleBlock.Delete
+				' oSheet.AddTitleBlock(oAddPageDef)
+				
 			' End If
 		' Next
 	' End If
 	
-	' If oModelDoc.DocumentType = kPartDocumentObject Then
-			' oModelDoc.PropertySets.Item("Design Tracking Properties").Item("Project").Value = ""
-	' End If
-		
-	'Dim oCtrlDef as ControlDefinitions = ThisApplication.CommandManager.ControlDefinitions
-	'oCtrlDef.Item("VaultCheckoutTop").Execute
+	' ' PropAdd(oDoc, "NextAssy", "")
+	' ' PropAdd(oDoc, "UsedOn", "")
+	' ' PropAdd(oModelDoc, "NextAssy", "")
+	' ' PropAdd(oModelDoc, "UsedOn", "")
+	' ' oDoc.PropertySets.Item("Design Tracking Properties").Item("Mfg Approved By").Value =  "K. Flagg"
+	' ' oModelDoc.PropertySets.Item("Design Tracking Properties").Item("Mfg Approved By").Value = "K. Flagg"
 	
-	SharedVariable("LogVar") = "SMLYDK Save as -FL"
-	iLogicVB.RunExternalRule("Write SV to Log.iLogicVB")
-	
-	'check file type 
-	
-	
-	Dim oOptions as NameValueMap = ThisApplication.TransientObjects.CreateNameValueMap
-		oOptions.Value("DeferUpdates") = True
-		oOptions.Value("FastOpen") = True
-		
-	'open the template
-	oTemplateDoc = ThisApplication.Documents.OpenWithOptions(oTemplate, oOptions, False)
-	
-	TitleBlockCopy(oTemplateDoc, oDoc, TBName)
-	'TitleBlockCopy(oTemplateDoc, oDoc, "UNIVERSAL BOURNE Kit Add")
-	BorderCopy(oTemplateDoc, oDoc, BdrName)
-	' SymbolCopy(oTemplateDoc, oDoc, "ACAD Leader Right")
-	' SymbolCopy(oTemplateDoc, oDoc, "ACAD Leader Left")
-	' SymbolCopy(oTemplateDoc, oDoc, "Univ. Part Notes")
-	' SymbolCopy(oTemplateDoc, oDoc, "TubeWeldTyp")
-	
-	' Dim IsKit = vbNo
-	' IsKit = MessageBox.Show("Is this a kit?","Kit?", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
-	' Try 
-		' If oModelDoc.PropertySets.Item("Inventor User Defined Properties").Item("DesignLevel") = "Kit" _
-		 ' OrElse oModelDoc.PropertySets.Item("Inventor User Defined Properties").Item("DesignLevel") = "Element" Then
-			' IsKit = MessageBox.Show("Is this a kit?","Kit?", MessageBoxButtons.YesNo,MessageBoxIcon.Question)
-		' End If 
-	' Catch
-	' End Try
-	
-	Dim oSheets As Sheets = oDoc.Sheets
-	Dim oSheet as Sheet = oDoc.Sheets.Item(1)
-	' Dim SheetCount = 1
-	
-	' 'if a kit, copy the general notes page, delete the current, and move the newly copied sheet to sheet 2
-	' If IsKit = vbYes Then
-		' oTemplateDoc.Sheets.Item(2).CopyTo(oDoc)
-		' oSheets.Item(2).Delete
-		
-		' Dim oBrowserPane as BrowserPane = oDoc.BrowserPanes.Item("Model")
-		' Dim ShtGenNotesNew As Sheet = oSheets.Item(oSheets.Count)
-		' Dim sheetNode As BrowserNode = oBrowserPane.GetBrowserNodeFromObject(ShtGenNotesNew)
-		' oBrowserPane.Reorder(oBrowserPane.GetBrowserNodeFromObject(oSheet), False, sheetNode)
-		
-	' End If
-	
-	oTemplateDoc.Close(True)
-	
-	oSheet.Activate
-	
-	Dim oBorderDef as BorderDefinition = oDoc.BorderDefinitions.Item(BdrName)
-	Dim oTitleTBDef as TitleBlockDefinition = oDoc.TitleBlockDefinitions.Item(TBName)
-	'Dim oAddPageDef as TitleBlockDefinition = oDoc.TitleBlockDefinitions.Item("UNIVERSAL BOURNE Kit Add")
-	
-	oSheet.Border.Delete
-	oSheet.TitleBlock.Delete
-	oSheet.AddBorder(oBorderDef)
-	oSheet.AddTitleBlock(oTitleTBDef)
+	' oDoc.Sheets.Item(1).Activate 'goto page 1
 
-	' If oDoc.AllReferencedDocuments.Item(1).DocumentType = kPartDocumentObject Then	
-		' InsertSketchedSymbolOnSheet(oDoc, "Univ. Part Notes")
-	' End If
-	
-	If oSheets.Count > 1 Then
-		
-		For Each oSheet In oSheets
-			If SheetCount < oSheets.Count Then
-				
-				SheetCount = SheetCount + 1
-				oSheet = oDoc.Sheets.Item(SheetCount)
-				oSheet.Activate
-				oSheet.Border.Delete
-				oSheet.AddBorder(oBorderDef)
-				oSheet.TitleBlock.Delete
-				oSheet.AddTitleBlock(oAddPageDef)
-				
-			End If
-		Next
-	End If
-	
-	' PropAdd(oDoc, "NextAssy", "")
-	' PropAdd(oDoc, "UsedOn", "")
-	' PropAdd(oModelDoc, "NextAssy", "")
-	' PropAdd(oModelDoc, "UsedOn", "")
-	' oDoc.PropertySets.Item("Design Tracking Properties").Item("Mfg Approved By").Value =  "K. Flagg"
-	' oModelDoc.PropertySets.Item("Design Tracking Properties").Item("Mfg Approved By").Value = "K. Flagg"
-	
-	oDoc.Sheets.Item(1).Activate 'goto page 1
-
-	oDoc.Save
+	' oDoc.Save
 	
 End Sub
 
